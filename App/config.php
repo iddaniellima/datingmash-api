368e7d5758a2ab8f33b4428d8c3e1f8adfb39101
iddaniellima <br.daniellima@outlook.com.br>
2019-05-02 00:06:33 +0000

    Configurações iniciais

---
+++ App/config.php
@@ -0,0 +1,15 @@
+<?php
+
+define('PROJECT_NAME', 'Datingmash');
+
+
+// CONFIGURAÇÕES MYSQL
+define('DB_USER', 'j4vYPCpEMz');
+define('DB_PASSWORD', 'O4Wp7Zqj54');
+define('DB_HOST', 'remotemysql.com');
+define('DB_NAME', 'j4vYPCpEMz');
+define('DB_PASSWORD','O4Wp7Zqj54');
+define('DB_PORT', 3306);
+
+// CONFIGURAÇÕES DE SEGURANÇA
+define('JWT_SIGNATURE', 'Xuy4fkiStijYhwOUadtkiKIIEtOwSXZ3');
